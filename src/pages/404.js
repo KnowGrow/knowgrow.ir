c3926e684d3e1ef5141ee2d3ccf518369240cc24
Mohsen <mohsenkhanpour@gmail.com>
2019-05-31 03:12:51 +0430

    new theme

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import Img from 'gatsby-image';
 
 import '@react-website-themes/classy-docs/styles/variables';
 import '@react-website-themes/classy-docs/styles/global';
 
+import Article from '@react-website-themes/classy-docs/components/Article';
+import Bodytext from '@react-website-themes/classy-docs/components/Bodytext';
 import Branding from '@react-website-themes/classy-docs/components/Branding';
 import Footer from '@react-website-themes/classy-docs/components/Footer';
 import Header from '@react-website-themes/classy-docs/components/Header';
-import Hero from '@react-website-themes/classy-docs/components/Hero';
+import Heading from '@react-website-themes/classy-docs/components/Heading';
 import Layout from '@react-website-themes/classy-docs/components/Layout';
 import Menu from '@react-website-themes/classy-docs/components/Menu';
 import Seo from '@react-website-themes/classy-docs/components/Seo';
@@ -16,15 +17,12 @@ import Seo from '@react-website-themes/classy-docs/components/Seo';
 import config from 'content/meta/config';
 import menuItems from 'content/meta/menu';
 
-const IndexPage = props => {
+const NotFoundPage = props => {
   const {
     data: {
+      notFound: { html: notFoundHTML },
       footerLinks: { html: footerLinksHTML },
-      hero: { html: heroHTML },
       copyright: { html: copyrightHTML },
-      logo: {
-        childImageSharp: { fluid: logoFluid },
-      },
     },
   } = props;
 
@@ -43,10 +41,10 @@ const IndexPage = props => {
         <Branding title={headerTitle} subTitle={headerSubTitle} />
         <Menu items={menuItems} />
       </Header>
-      <Hero>
-        <Img fluid={logoFluid} className="image" />
-        <div dangerouslySetInnerHTML={{ __html: heroHTML }} />
-      </Hero>
+      <Article>
+        <Heading title="NOT FOUND" />
+        <Bodytext html={notFoundHTML} />
+      </Article>
       <Footer links={footerLinksHTML} copyright={copyrightHTML} />
       <Seo
         url={siteUrl}
@@ -58,18 +56,13 @@ const IndexPage = props => {
   );
 };
 
-export default IndexPage;
+export default NotFoundPage;
 
 export const query = graphql`
   query {
-    logo: file(relativePath: { regex: "/logo.png/" }) {
-      childImageSharp {
-        fluid(maxWidth: 500) {
-          ...GatsbyImageSharpFluid_withWebp_tracedSVG
-        }
-      }
-    }
-    hero: markdownRemark(fileAbsolutePath: { regex: "/content/parts/hero/" }) {
+    notFound: markdownRemark(
+      fileAbsolutePath: { regex: "/content/parts/notFound/" }
+    ) {
       html
     }
     footerLinks: markdownRemark(
--- src/pages/content.js
+++ src/pages/content.js
@@ -1,30 +1,29 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import Img from 'gatsby-image';
 
 import '@react-website-themes/classy-docs/styles/variables';
 import '@react-website-themes/classy-docs/styles/global';
 
+import Article from '@react-website-themes/classy-docs/components/Article';
 import Branding from '@react-website-themes/classy-docs/components/Branding';
 import Footer from '@react-website-themes/classy-docs/components/Footer';
 import Header from '@react-website-themes/classy-docs/components/Header';
-import Hero from '@react-website-themes/classy-docs/components/Hero';
+import Heading from '@react-website-themes/classy-docs/components/Heading';
 import Layout from '@react-website-themes/classy-docs/components/Layout';
+import List from '@react-website-themes/classy-docs/components/List';
 import Menu from '@react-website-themes/classy-docs/components/Menu';
 import Seo from '@react-website-themes/classy-docs/components/Seo';
 
 import config from 'content/meta/config';
 import menuItems from 'content/meta/menu';
+import categoryList from 'content/meta/categories';
 
-const IndexPage = props => {
+const ContentPage = props => {
   const {
     data: {
+      pages: { edges: rawPages },
       footerLinks: { html: footerLinksHTML },
-      hero: { html: heroHTML },
       copyright: { html: copyrightHTML },
-      logo: {
-        childImageSharp: { fluid: logoFluid },
-      },
     },
   } = props;
 
@@ -37,16 +36,18 @@ const IndexPage = props => {
     siteLanguage,
   } = config;
 
+  const pages = rawPages.map(page => page.node);
+
   return (
     <Layout>
       <Header>
         <Branding title={headerTitle} subTitle={headerSubTitle} />
         <Menu items={menuItems} />
       </Header>
-      <Hero>
-        <Img fluid={logoFluid} className="image" />
-        <div dangerouslySetInnerHTML={{ __html: heroHTML }} />
-      </Hero>
+      <Article>
+        <Heading title="Table of content" />
+        <List pages={pages} categoryList={categoryList} />
+      </Article>
       <Footer links={footerLinksHTML} copyright={copyrightHTML} />
       <Seo
         url={siteUrl}
@@ -58,14 +59,25 @@ const IndexPage = props => {
   );
 };
 
-export default IndexPage;
+export default ContentPage;
 
 export const query = graphql`
   query {
-    logo: file(relativePath: { regex: "/logo.png/" }) {
-      childImageSharp {
-        fluid(maxWidth: 500) {
-          ...GatsbyImageSharpFluid_withWebp_tracedSVG
+    pages: allMarkdownRemark(
+      filter: { fields: { source: { eq: "docs" } } }
+      sort: { fields: [fields___prefix] }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+            prefix
+          }
+          frontmatter {
+            title
+            shortTitle
+            categories
+          }
         }
       }
     }
